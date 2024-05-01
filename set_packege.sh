77e3408806c4cfd6f8ab04dc91536568921fce97
motii8128 <default270robo@gmail.com>
2024-05-02 08:41:36 +0900

    Update

--- set_packege.sh
+++ set_packege.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 mkdir src
+echo "Install moti auto repos."
 vcs import src < motiauto.repos
 
+echo "Install thirdparty package."
+
 source /opt/ros/humble/setup.bash
+rosdep update
 rosdep install -y --from-paths src --ignore-src --rosdistro $ROS_DISTRO
+rosdep update
 
 colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release --cargo-args --release
